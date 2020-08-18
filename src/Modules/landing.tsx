3e2be281eced77a8f3a6150e4afc3d5b16153c7e
Ankit Tiwari <ankit.tiwari@drivezy.com>
2020-08-18 21:55:18 +0530

    #comment

--- src/Modules/landing.tsx
+++ src/Modules/landing.tsx
@@ -2,44 +2,40 @@ import React, { useState } from "react";
 import { Payment, SubscribeToEvent } from "shopkonnect-frontend-business";
 
 const BaseModule = () => {
-  let user: { id?: string };
-  const [financeCardProps, setFinanceProps] = useState([]);
-  // const [testing, setTestProps] = useState("ankit");
-  // const [financeSummaryData, setFinanceSummaryData] = useState([]);
+    let user: { id?: string };
+    // const [financeCardProps, setFinanceProps] = useState([]);
+    const [financeSummaryData, setFinanceSummaryData] = useState([]);
 
-  React.useEffect(() => {
-    SubscribeToEvent({
-      eventName: "USER",
-      callback: (user) => proceedApiCall(user),
-    });
-    // const test = { ankit: 1, ashish: 1 };
+    React.useEffect(() => {
+        SubscribeToEvent({
+            eventName: "USER",
+            callback: (user) => proceedApiCall(user),
+        });
+    }, []);
 
-    // fetchPaymentSummary(paymentMethod);
-  }, []);
+    const proceedApiCall = (userObject) => {
+        if (userObject.id) {
+            user = userObject;
+            const paymentMethod = new Payment();
+            fetchPaymentStats(paymentMethod);
+        }
+    };
 
-  const proceedApiCall = (userObject) => {
-    if (userObject.id) {
-      user = userObject;
-      const paymentMethod = new Payment();
-      fetchPaymentStats(paymentMethod);
-    }
-  };
+    const fetchPaymentStats = async (paymentMethod) => {
+        const result = await paymentMethod.fetchSummary(user);
+        setFinanceSummaryData(result);
+    };
 
-  const fetchPaymentStats = async (paymentMethod) => {
-    const result = await paymentMethod.fetchSummary(user);
-    setFinanceProps(result);
-  };
+    // const test = async (paymentMethod) => {
+    //   const result = ["ankit", "ashish"];
+    //   setTestProps("ankit");
+    // };
 
-  // const test = async (paymentMethod) => {
-  //   const result = ["ankit", "ashish"];
-  //   setTestProps("ankit");
-  // };
-
-  // const fetchPaymentSummary = async (paymentMethod) => {
-  //   const result = await paymentMethod.fetchUserAccountSummary();
-  //   setFinanceSummaryData(result);
-  // };
-  return <h1>{fetchPaymentStats}</h1>;
+    // const fetchPaymentSummary = async (paymentMethod) => {
+    //   const result = await paymentMethod.fetchUserAccountSummary();
+    //   setFinanceSummaryData(result);
+    // };
+    return <h1>Hi</h1>;
 };
 
 export default BaseModule;
--- src/Router/base.router.tsx
+++ src/Router/base.router.tsx
@@ -1,23 +1,23 @@
 import React from "react";
 import {
-  InitLibrary,
-  user,
-  Authentication,
+    InitLibrary,
+    user,
+    Authentication,
 } from "shopkonnect-frontend-business";
 import { SubscribeToEvent, StoreEvent } from "state-manager-utility";
 import { GetItem, SetItem, RemoveItem, GetItemAsync } from "storage-utility";
 import Landing from "./../Modules/landing";
 
 const BaseRouter = () => {
-  InitLibrary({
-    apiHost: "https://sk-api-services.herokuapp.com/",
-    StateManager: { SubscribeToEvent, StoreEvent },
-    StorageUtility: { GetItem, SetItem, RemoveItem, GetItemAsync },
-  });
-  Authentication.mocklogin();
-  // const user = new User();
+      InitLibrary({
+        apiHost: "https://sk-api-services.herokuapp.com/",
+        StateManager: { SubscribeToEvent, StoreEvent },
+        StorageUtility: { GetItem, SetItem, RemoveItem, GetItemAsync },
+      });
+     Authentication.mocklogin();
+    // const user = new User();
 
-  return <Landing />;
+    return <Landing />;
 };
 
 export default BaseRouter;
