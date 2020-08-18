abcd797083acc9299717d670ac0ce2a97e3d0c88
Ankit Tiwari <ankit.tiwari@drivezy.com>
2020-08-18 16:15:23 +0530

    #COMMENT PUSHED EVERYTHING

--- src/Modules/landing.tsx
+++ src/Modules/landing.tsx
@@ -1,14 +1,45 @@
-import React from "react";
-import { Payment } from "shopkonnect-frontend-business";
+import React, { useState } from "react";
+import { Payment, SubscribeToEvent } from "shopkonnect-frontend-business";
 
 const BaseModule = () => {
+  let user: { id?: string };
+  const [financeCardProps, setFinanceProps] = useState([]);
+  // const [testing, setTestProps] = useState("ankit");
+  // const [financeSummaryData, setFinanceSummaryData] = useState([]);
+
   React.useEffect(() => {
-    const paymentMethod = new Payment();
-    fetchPaymentStats(paymentMethod);
-    fetchPaymentSummary(paymentMethod);
+    SubscribeToEvent({
+      eventName: "USER",
+      callback: (user) => proceedApiCall(user),
+    });
+    // const test = { ankit: 1, ashish: 1 };
+
+    // fetchPaymentSummary(paymentMethod);
   }, []);
 
-  return <h1>Module</h1>;
+  const proceedApiCall = (userObject) => {
+    if (userObject.id) {
+      user = userObject;
+      const paymentMethod = new Payment();
+      fetchPaymentStats(paymentMethod);
+    }
+  };
+
+  const fetchPaymentStats = async (paymentMethod) => {
+    const result = await paymentMethod.fetchSummary(user);
+    setFinanceProps(result);
+  };
+
+  // const test = async (paymentMethod) => {
+  //   const result = ["ankit", "ashish"];
+  //   setTestProps("ankit");
+  // };
+
+  // const fetchPaymentSummary = async (paymentMethod) => {
+  //   const result = await paymentMethod.fetchUserAccountSummary();
+  //   setFinanceSummaryData(result);
+  // };
+  return <h1>{fetchPaymentStats}</h1>;
 };
 
 export default BaseModule;
--- src/Router/base.router.tsx
+++ src/Router/base.router.tsx
@@ -1,17 +1,22 @@
 import React from "react";
-import { InitLibrary } from "shopkonnect-frontend-business";
+import {
+  InitLibrary,
+  user,
+  Authentication,
+} from "shopkonnect-frontend-business";
 import { SubscribeToEvent, StoreEvent } from "state-manager-utility";
 import { GetItem, SetItem, RemoveItem, GetItemAsync } from "storage-utility";
 import Landing from "./../Modules/landing";
 
 const BaseRouter = () => {
-  React.useEffect(() => {
-    InitLibrary({
-      apiHost: "https://sk-api-services.herokuapp.com",
-      StateManager: { SubscribeToEvent, StoreEvent },
-      StorageUtility: { GetItem, SetItem, RemoveItem, GetItemAsync },
-    });
-  }, []);
+  InitLibrary({
+    apiHost: "https://sk-api-services.herokuapp.com/",
+    StateManager: { SubscribeToEvent, StoreEvent },
+    StorageUtility: { GetItem, SetItem, RemoveItem, GetItemAsync },
+  });
+  Authentication.mocklogin();
+  // const user = new User();
+
   return <Landing />;
 };
 
